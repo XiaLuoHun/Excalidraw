baa128436942eed89f3b74c112c1d2476f30ee38
XiaLuoHun <1305002420@qq.com>
2024-01-22 10:39:57 +0800

    excalidraw 2024.01.22 update

---
+++ assets/selectAll-RrXOwD-e.js
@@ -0,0 +1,2 @@
+import{a1 as t,a2 as n,a3 as o}from"./index-41k886O4.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
+//# sourceMappingURL=selectAll-RrXOwD-e.js.map
